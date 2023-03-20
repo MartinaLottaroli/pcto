fc26c7136dddd7041c7a5e59d1f3b1beeeec730b
MartinaLottaroli <128370021+MartinaLottaroli@users.noreply.github.com>
2023-03-20 12:38:12 +0100

    Add files via upload

---
+++ ex00.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main () 
+{
+    float x = 28;
+    char a = 'b';
+    char b = 'c';
+    int c = 109;
+
+    printf("ho creato una variabile di tipo float con valore %f, il primo carattere è %c, il secondo carattere è %c, ultimo int %c\n", x, a, b, c);
+}
