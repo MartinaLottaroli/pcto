66b3dc99aa1720bf192dc6b654667ee70d026e8c
MartinaLottaroli <128370021+MartinaLottaroli@users.noreply.github.com>
2023-03-20 15:59:06 +0100

    Add files via upload

---
+++ ex06.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    printf("inserisci un numero");
+    scanf("%d", &x);
+
+    if(x>50)
+    {
+        x/=2;
+      printf("il mio valore è %d\n", x);
+       
+    }
+   else
+   {
+    x*=3;
+    printf("il mio valore è %d\n", x);
+   }
+}
