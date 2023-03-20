3e2fbb3629511e34589ee79446366afa435f082d
MartinaLottaroli <128370021+MartinaLottaroli@users.noreply.github.com>
2023-03-20 15:33:17 +0100

    Add files via upload

---
+++ ex05.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+ 
+ int main()
+ {
+    int x;
+    printf("inserisci un età");
+    scanf("%d", &x);
+
+    if(x>18)
+    {
+        printf("sei maggiorenne");
+    }
+    else
+    {
+        printf("sei minorenne");
+    }
+ }
