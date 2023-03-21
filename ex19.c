2a314db163fa82f7211618a62f7c9afa4ef3db6a
MartinaLottaroli <128370021+MartinaLottaroli@users.noreply.github.com>
2023-03-21 15:19:14 +0100

    Add files via upload

---
+++ ex19.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+    int x=0;
+    while(x<100)
+    {
+        if(x<10)
+        {
+            printf("0%d\n", x);
+        }
+        else
+        {
+            printf("%d\n", x);
+        }
+        x= x+1;
+    }
+}
