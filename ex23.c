0c189d0f09fd5395120032ce85d3995406f9eb34
MartinaLottaroli <128370021+MartinaLottaroli@users.noreply.github.com>
2023-03-22 09:53:45 +0100

    Add files via upload

--- ex23.c
+++ ex23.c
@@ -5,10 +5,13 @@ int main()
     int x;
     int y;
     int z;
+    int f;
+    int media;
   
     z=0;
     printf("inserisci un numero positivo");
     scanf("%d", &x);
+    f= x;
 
     while(x>0)
    {
@@ -17,6 +20,10 @@ int main()
     z=z+y;
     x=x-1;
    }
+
+   printf("la somma di tutti numeri che ho inserito è %d\n", z);
+   media= z/f;
+   printf("la media dei numeri è %d\n", media);
      
-    printf("la somma di tutti i numeri che ho inserito %d\n", z);
+   printf("la somma di tutti i numeri che ho inserito %d\n", z);
 }
